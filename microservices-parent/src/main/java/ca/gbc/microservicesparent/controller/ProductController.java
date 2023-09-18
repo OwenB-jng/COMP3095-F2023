aa03fb1e919f7cba5bdb003d9fb40479458bfd96
OwenB-jng <owenbeattie1@gmail.com>
2023-09-18 08:33:38 -0400

    proper directories

--- microservices-parent/src/main/java/ca/gbc/microservicesparent/controller/ProductController.java
+++ microservices-parent/src/main/java/ca/gbc/microservicesparent/controller/ProductController.java
@@ -1,4 +1,11 @@
 package ca.gbc.microservicesparent.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/product")
 public class ProductController {
+
 }
